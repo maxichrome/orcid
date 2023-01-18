25a4c7fbfa721f3e79311ca3687341438135abb2
maxichrome <maxichrome@users.noreply.github.com>
2023-01-18 08:04:02 -0800

    remove transcription object log

--- src/lib/ocr.ts
+++ src/lib/ocr.ts
@@ -2,7 +2,6 @@ import { imageAnnotator } from '../client/gcp'
 
 export async function transcribeImageText(url: string): Promise<string | null> {
 	const [result] = await imageAnnotator.textDetection(url)
-	console.log(JSON.stringify(result.fullTextAnnotation))
 
 	return result.fullTextAnnotation?.text ?? null
 }
