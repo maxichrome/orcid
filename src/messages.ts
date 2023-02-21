47a004ac15e7e4a06fd82429a14f030caa5f8665
maxichrome <git+github@maxichrome.dev>
2023-02-21 00:23:05 -0600

    use single line result for truncation

--- src/messages.ts
+++ src/messages.ts
@@ -3,9 +3,9 @@ import { generateCopyUrl } from './lib/copy'
 const RESULT_PREVIEW_TRUNCATE_LIMIT = 140
 export const resultBody = (result: string) => {
 	const copyUrl = generateCopyUrl(result)
-	const isTruncated = result.length > RESULT_PREVIEW_TRUNCATE_LIMIT
-
 	const singleLineResult = result.split('\n').join(' ')
+
+	const isTruncated = singleLineResult.length > RESULT_PREVIEW_TRUNCATE_LIMIT
 	const displayResult = isTruncated
 		? `${singleLineResult.substring(0, RESULT_PREVIEW_TRUNCATE_LIMIT)} …`
 		: singleLineResult
